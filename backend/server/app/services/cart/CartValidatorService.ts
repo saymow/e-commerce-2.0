10a11d4b4576bc53ab9db4591b0ffa2a4d0e4bbd
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-01-17 19:16:54 -0300

    test: ensure CartValidator returns true on valid data

---
+++ backend/server/app/services/cart/CartValidatorService.ts
@@ -0,0 +1,47 @@
+import { Cart } from '@services/checkout/CheckoutService';
+import * as yup from 'yup';
+
+const productSchema = yup.object({
+  id: yup.string().uuid().required(),
+  qty: yup.number().integer().min(1).required(),
+});
+
+const shipmentMethodSchema = yup.object({
+  code: yup.string().required(),
+  name: yup.string().required(),
+  value: yup.number().integer().required(),
+  deadline: yup.string().required(),
+  postalCode: yup
+    .string()
+    .matches(/^\d{5}-\d{3}$/)
+    .required(),
+});
+
+const shipmentAddressSchema = yup.object({
+  state: yup.string().required(),
+  city: yup.string().required(),
+  neighborhood: yup.string().required(),
+  postal_code: yup
+    .string()
+    .matches(/^\d{5}-\d{3}$/)
+    .required(),
+  street: yup.string().required(),
+  number: yup.number().integer().required(),
+});
+
+const validationSchema = yup.object({
+  products: yup.array().of(productSchema).required(),
+  shipmentMethod: shipmentMethodSchema.required(),
+  total: yup.number().integer().required(),
+  subtotal: yup.number().integer().required(),
+  shippingCost: yup.number().integer().required(),
+  shipmentAddress: shipmentAddressSchema.required(),
+});
+
+class CartValidatorService {
+  async execute(cart: Cart) {
+    return true;
+  }
+}
+
+export default CartValidatorService;
