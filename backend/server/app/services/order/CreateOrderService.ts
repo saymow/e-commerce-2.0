56c80362b90f40dd836fb1bf0798c0a9e934c418
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-01-18 15:11:31 -0300

    feat: ensure CreateOrderService calls CartValidatorService with correct values

--- backend/server/app/services/order/CreateOrderService.ts
+++ backend/server/app/services/order/CreateOrderService.ts
@@ -1,12 +1,20 @@
-import { Cart } from "@services/checkout/CheckoutService";
+import CartValidatorService from '@services/cart/CartValidatorService';
+import { Cart } from '@services/checkout/CheckoutService';
 
+export interface OrderPayload {
+  cart: Cart;
+  userId: string;
+  checkoutId: string;
+  paymentId: string;
+  paymentSource: string;
+}
 
 class CreateOrderService {
-  async execute(
-    cart: Cart,
-    userId: string,
-    checkoutId: string,
-    paymentId: string,
-    paymentSource: string
-  ) {}
+  constructor(private readonly cartValidatorService: CartValidatorService) {}
+
+  async execute(orderPayload: OrderPayload) {
+    await this.cartValidatorService.execute(orderPayload.cart);
+  }
 }
+
+export default CreateOrderService;
